eed08f74a32631ab82b10b29a67569849bf4fb63
Luka Domitrovic <luka@domitrovic.si>
2013-01-28 10:07:33 +0100

    image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image, :name, :price
+  attr_accessible :description, :image, :name, :price, :photo
 
   validates :name, :presence => true
   validates :price, :presence => true, :numericality => true
+
+  has_attached_file :photo
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217080114) do
+ActiveRecord::Schema.define(:version => 20130128082956) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image"
     t.decimal  "price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
